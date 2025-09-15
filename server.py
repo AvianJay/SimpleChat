a147f1cbba07616eaf77b80bc1984555b2274179
AvianJay <jay20090728@gmail.com>
2025-09-15 12:46:20 +0800

    add page

--- server.py
+++ server.py
@@ -20,27 +20,33 @@ def get_request_data(request):
         reqdata = request.args.copy()
     return reqdata
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    data = get_request_data(request)
-    if not data or 'name' not in data or 'email' not in data or 'password' not in data:
-        return {'error': 'Invalid input'}, 400
-    user_id = database.create_user(conn, data['name'], data['email'], data['password'])
-    return {'message': 'User registered', 'user_id': user_id}, 201
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        data = get_request_data(request)
+        if not data or 'name' not in data or 'email' not in data or 'password' not in data:
+            return {'error': 'Invalid input'}, 400
+        user_id = database.create_user(conn, data['name'], data['email'], data['password'])
+        return {'message': 'User registered', 'user_id': user_id}, 201
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    data = get_request_data(request)
-    if not data or 'email' not in data or 'password' not in data:
-        return {'error': 'Invalid input'}, 400
-    user = database.get_user(conn, email=data['email'])
-    if user and user[3] == hashlib.sha256(data['password'].encode()).hexdigest():
-        return {'message': 'Login successful', 'token': user[4]}, 200
-    return {'error': 'Invalid credentials'}, 401
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        data = get_request_data(request)
+        if not data or 'email' not in data or 'password' not in data:
+            return {'error': 'Invalid input'}, 400
+        user = database.get_user(conn, email=data['email'])
+        if user and user[3] == hashlib.sha256(data['password'].encode()).hexdigest():
+            return {'message': 'Login successful', 'token': user[4]}, 200
+        return {'error': 'Invalid credentials'}, 401
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 def run():
     app.run()
