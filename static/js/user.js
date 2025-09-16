c0f3c4ebd06fda8aa644c71c41d87a58448a75fc
AvianJay <jay20090728@gmail.com>
2025-09-16 10:57:40 +0800

    add login script

--- static/js/user.js
+++ static/js/user.js
@@ -1,3 +1,19 @@
 function login() {
-    // todo
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value;
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: username, password: password })
+    }).then(response => response.json())
+        .then(data => {
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                window.location.href = '/chat';
+            } else {
+                alert('Login failed: ' + data.error);
+            }
+        });
 }
